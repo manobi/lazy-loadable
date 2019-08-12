48d24d69dec9f1f14b78a9363921cd02f34a8cc2
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2019-08-12 08:11:30 -0300

    native lazyload

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
-# &lt;custom-element&gt; element
+# Native lazy load boilerplate
 
-Boilerplate for creating a custom element.
+* 🧱 Pollyfill for modern browsers not supporting it yet
+* 👵 Gracefully degrades for old browsers
+* 🤖 Future proof markup
+* 🚉 Using the platform
+
+The whole idea is to create a copy-past boilerplate that works today. If by a miracle all browser vendors agree to ship native lazy load, you would **only have to change one place**.
+
+## Boilerplate
+```html
+<!-- 1. Markup -->
+<img 
+    is="lazy-loadable" 
+    loading="lazy" 
+    lazyload="1" 
+    importance="low" 
+    srcset="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAEElEQVR42gEFAPr/AP///wAI/AL+Sr4t6gAAAABJRU5ErkJggg==" 
+    src="https://placekitten.com/400/400" 
+    width="400" 
+    height="400" 
+    alt=""
+>
+<!-- 2. Safari - Custom elements pollyful -->
+<script>
+    if(this.customElements)
+    try{customElements.define('built-in',document.createElement('p').constructor,{'extends':'p'})}
+    catch(s){document.write('<script src="//unpkg.com/@ungap/custom-elements-builtin"><\x2fscript>')}
+    else
+    document.write('<script src="//unpkg.com/document-register-element"><\x2fscript>');
+</script>
+<!-- 3. Img HTML extension -->
+<script type="module">
+    import LazyLoadable from '../index.js';
+    customElements.define('lazy-loadable', LazyLoadable, { extends: "img" });
+</script>
+```
+
+
+## Custom elements
+custom elements instead of querySelectorAll cuz no forEach needed
+better for inifinity scroll when images are not present when the for each loads
+## src-set instead of data-src use the platform, because removing it will fallback to browser default behaviour
+## In future all we would
+## data-src swap techinique is not exactly compatible with picture source
+##
+lazyload="1" importance="low"
+
+
+  
 
 ## Installation
 
--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-/* @flow strict */
-
-class CustomElementElement extends HTMLElement {
-  constructor() {
-    super()
-  }
-
-  connectedCallback() {
-    this.textContent = ':wave:'
-  }
-
-  disconnectedCallback() {}
+/**
+ * Lazyload component
+ * IE &  edge: https://msdn.microsoft.com/en-us/ie/dn369270(v=vs.94)
+ */
+const opts = {
+  // The root to use for intersection.
+  // If not provided, use the top-level documentÃ¢â‚¬â„¢s viewport.
+  root: null,
+  // Same as margin, can be 1, 2, 3 or 4 components, possibly negative lengths.
+  // If an explicit root element is specified, components may be percentages of the
+  // root element size.  If no explicit root element is specified, using a percentage
+  // is an error.
+  rootMargin: "0px",
+  // Threshold(s) at which to trigger callback, specified as a ratio, or list of
+  // ratios, of (visible area / total area) of the observed element (hence all
+  // entries must be in the range [0, 1]).  Callback will be invoked when the visible
+  // ratio of the observed element crosses a threshold in the list.
+  threshold: [0]
 }
 
-export default CustomElementElement
+const toPrefetch = new Set();
+function prefetch(url) {
+  toPrefetch.add(url)
+  fetch(url, {credentials: `include`, cache: 'force-cache'}).then(async res => await res.blob())
+}
 
-if (!window.customElements.get('custom-element')) {
-  window.CustomElementElement = CustomElementElement
-  window.customElements.define('custom-element', CustomElementElement)
+function callback(entries){
+  entries.forEach((entry) => {
+      if(entry.isIntersecting && !entry.target.getAttribute('data-swapeded')){
+        const link = entry.target.getAttribute('link');
+        if(link){
+          entry.target.addEventListener('load', () => prefetch(link), {once: true});
+          entry.target.removeAttribute('link');
+        }
+        entry.target.removeAttribute('srcset');
+        entry.target.setAttribute('data-swapeded', true);
+        
+        //this.disconnect(entry.target);
+      }
+  });
+}
+
+const io = new IntersectionObserver(callback, opts);
+
+export default class LazyLoadable extends HTMLImageElement {
+  constructor(width, height){
+    super(width, height);
+  }
+
+  get isModern(){
+    return ('loading' in HTMLImageElement.prototype)
+  }
+
+  connectedCallback(){
+    if(!this.isModern){
+      return io.observe(this);
+    }
+    this.removeAttribute('srcset');
+  }
+
+  disconnectedCallback(){
+    io.disconnect(this);
+  }
 }
