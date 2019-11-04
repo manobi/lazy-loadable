06affa02a59e982986c87cadad81923a9d30a354
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2019-11-03 23:19:42 -0300

    refactoring IntersectionObserver

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
 const opts = {
-  // The root to use for intersection.
-  // If not provided, use the top-level documentÃ¢â‚¬â„¢s viewport.
   root: null,
-  // Same as margin, can be 1, 2, 3 or 4 components, possibly negative lengths.
-  // If an explicit root element is specified, components may be percentages of the
-  // root element size.  If no explicit root element is specified, using a percentage
-  // is an error.
   rootMargin: "0px",
-  // Threshold(s) at which to trigger callback, specified as a ratio, or list of
-  // ratios, of (visible area / total area) of the observed element (hence all
-  // entries must be in the range [0, 1]).  Callback will be invoked when the visible
-  // ratio of the observed element crosses a threshold in the list.
   threshold: [0]
 }
 
-const toPrefetch = new Set();
-function prefetch(url) {
-  toPrefetch.add(url)
-  fetch(url, {credentials: `include`, cache: 'force-cache'}).then(async res => await res.blob())
+function prefetch(url) {  
+  return fetch(url, {credentials: `include`, cache: 'force-cache'}).then(async res => await res.blob())
 }
 
-function callback(entries){
-  entries.forEach((entry) => {
-      if(entry.isIntersecting && !entry.target.getAttribute('data-swapeded')){
-        const link = entry.target.getAttribute('link');
-        if(link){
-          entry.target.addEventListener('load', () => prefetch(link), {once: true});
-          entry.target.removeAttribute('link');
-        }
-        entry.target.removeAttribute('srcset');
-        entry.target.setAttribute('data-swapeded', true);
-        
-        //this.disconnect(entry.target);
+function loadSrc(target, io){
+  target.removeAttribute('srcset');
+  io.unobserve(target);
+}
+
+const io = new IntersectionObserver((entries) => {
+  entries.forEach(({isIntersecting, target}) => {
+    if(isIntersecting){
+      loadSrc(target, io);
+      const link = target.getAttribute('link');
+      if(link){
+        target.addEventListener('load', () => prefetch(link), {once: true});
       }
-  });
-}
-
-const io = new IntersectionObserver(callback, opts);
+    }
+  })
+}, opts);
 
 export default class LazyLoadable extends HTMLImageElement {
   constructor(width, height){
     super(width, height);
+  }
+
+  get loading(){
+    return this.getAttribute('loading')
   }
 
   get isModern(){
@@ -48,13 +39,14 @@ export default class LazyLoadable extends HTMLImageElement {
   }
 
   connectedCallback(){
-    if(!this.isModern){
+    if(this.loading === 'lazy'){
       return io.observe(this);
     }
-    this.removeAttribute('srcset');
+    // default behaviour is to eagerly load the image
+    loadSrc(this);
   }
 
   disconnectedCallback(){
-    io.disconnect(this);
+    io.unobserve(this);
   }
 }
